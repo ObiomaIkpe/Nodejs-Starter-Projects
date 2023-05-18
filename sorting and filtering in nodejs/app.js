a86940093e6a5d7010bbb6ff90d9cfd4d49a4bde
ObiomaIkpe <obiomaikpe1.com>
2023-05-18 20:03:26 +0100

    Nodejs projects

--- sorting and filtering in nodejs/app.js
+++ sorting and filtering in nodejs/app.js
@@ -1,1 +1,19 @@
-console.log('04 Store API')
+const express =  require('express');
+const app = express()
+const connectDB = require('./db/connect')
+
+
+
+
+
+
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
